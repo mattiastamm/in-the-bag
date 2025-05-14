8c96bf66dcf03d867c4e09b475479fc44ae040a5
mattiastamm <johannmattias@gmail.com>
2025-05-14 15:55:31 +0300

    Minor changes to CorsConfig

--- in-the-bag-backend/src/main/java/com/discgolf/in_the_bag/config/SecurityConfig.java
+++ in-the-bag-backend/src/main/java/com/discgolf/in_the_bag/config/SecurityConfig.java
@@ -57,9 +57,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Frontend
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         config.setExposedHeaders(List.of("Authorization"));
 
